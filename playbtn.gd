b2179d679e7a44fe6b30415c2e7967802fe0d5c2
Connor061292 <connorgibson061209@gmail.com>
2025-03-28 21:40:23 -0500

    added scripts

---
+++ playbtn.gd
@@ -0,0 +1,6 @@
+extends Button
+func _on_button_pressed():
+	# Called when the button is pressed.
+
+	# Load the scene you want to switch to.
+	var scene = load("res://scene_one.tscn")  # Replace with the correct path if needed
